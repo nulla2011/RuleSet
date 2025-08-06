d49a77ae272d9a115ee363259987a256dce5f891
nulla2011 <nulla@outlook.com>
2025-08-06 19:46:08 +0800

    update claude

--- Script/ai_sites.mjs
+++ Script/ai_sites.mjs
@@ -2,9 +2,10 @@ import {readFileSync, writeFileSync} from "fs";
 import {parse, stringify} from "yaml";
 
 const main = () => {
-  let OpenAIRules, GeminiRules, rm, shared
+  let OpenAIRules, ClaudeRules, GeminiRules, rm, shared
   try {
     OpenAIRules = parse(readFileSync(`../Temp/OpenAI.yaml`, "utf8"));
+    ClaudeRules = parse(readFileSync(`../Temp/Claude.yaml`, "utf8"));
     GeminiRules = {payload: parse(readFileSync(`../Patches/Add/Gemini.yaml`, "utf8"))};
     rm = parse(readFileSync(`../Patches/Remove/AI.yaml`, "utf8"));
     shared = parse(readFileSync(`../Patches/Remove/Shared.yaml`, "utf8"));
@@ -12,7 +13,7 @@ const main = () => {
   catch (e) {
     console.error(e)
   }
-  const rules = {payload: OpenAIRules.payload.concat(GeminiRules.payload)}
+  const rules = {payload: OpenAIRules.payload.concat(GeminiRules.payload).concat(ClaudeRules.payload)}
   rules.payload = rules.payload.filter((r)=> !rm.some((e)=> r.includes(e)))
   rules.payload = rules.payload.filter((r)=> !shared.some((e)=> r.includes(e)))
   try {
---
+++ Script/proxy.mjs
@@ -0,0 +1,3 @@
+import {readFileSync, writeFileSync} from "fs";
+import {parse, stringify} from "yaml";
+
