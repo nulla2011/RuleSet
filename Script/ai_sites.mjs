138f4003d8b3bbd19b9476ef14ab70f08667316d
nulla2011 <nulla@outlook.com>
2025-08-07 15:32:19 +0800

    fix ai

--- Script/ai_sites.mjs
+++ Script/ai_sites.mjs
@@ -1,19 +1,16 @@
-import {readFileSync, writeFileSync} from "fs";
-import {parse, stringify} from "yaml";
+import { readFileSync, writeFileSync } from "fs";
+import { parse, stringify } from "yaml";
 
 const main = () => {
-  let OpenAIRules, ClaudeRules, GeminiRules, rm, shared
+  let rules, rm, shared
   try {
-    OpenAIRules = parse(readFileSync(`../Temp/OpenAI.yaml`, "utf8"));
-    ClaudeRules = parse(readFileSync(`../Temp/Claude.yaml`, "utf8"));
-    GeminiRules = {payload: parse(readFileSync(`../Patches/Add/Gemini.yaml`, "utf8"))};
+    rules = { payload: readFileSync('../Temp/ai.conf', 'utf-8').split('\n').filter(t => !t.startsWith('#')) }
     rm = parse(readFileSync(`../Patches/Remove/AI.yaml`, "utf8"));
     shared = parse(readFileSync(`../Patches/Remove/Shared.yaml`, "utf8"));
   }
   catch (e) {
     console.error(e)
   }
-  const rules = {payload: OpenAIRules.payload.concat(GeminiRules.payload).concat(ClaudeRules.payload)}
   rules.payload = rules.payload.filter(r => !rm.some(e => r.includes(e)))
   rules.payload = rules.payload.filter(r => !shared.some(e => r.includes(e)))
   try {
