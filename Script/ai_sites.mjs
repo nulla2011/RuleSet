6fd605d27e6095d728a10d1701bbfb5111a88693
nulla2011 <nulla@outlook.com>
2025-08-31 11:38:52 +0800

    fix

--- Script/ai_sites.mjs
+++ Script/ai_sites.mjs
@@ -2,19 +2,19 @@ import { readFileSync, writeFileSync } from "fs";
 import { parse, stringify } from "yaml";
 
 const main = () => {
-  let rules, rm, shared
+  let rules, rm, shared, add
   try {
     rules = { payload: readFileSync('../Temp/ai.txt', 'utf-8').split('\n').filter(t => t && !t.startsWith('#')) }
-    add = parse(readFileSync(`../Patches/Add/AI.yaml`, "utf8"));
     rm = parse(readFileSync(`../Patches/Remove/AI.yaml`, "utf8"));
     shared = parse(readFileSync(`../Patches/Remove/Shared.yaml`, "utf8"));
+    add = parse(readFileSync(`../Patches/Add/AI.yaml`, "utf8"));
   }
   catch (e) {
     console.error(e)
   }
-  rules.payload = rules.payload.concat(add)
   rules.payload = rules.payload.filter(r => !rm.some(e => r.includes(e)))
   rules.payload = rules.payload.filter(r => !shared.some(e => r.includes(e)))
+  rules.payload = rules.payload.concat(add)
   try {
     writeFileSync(`../Rules/AI.yaml`, stringify(rules), "utf8");
   }
