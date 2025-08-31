5ba442cd3031424f8aada2553a1ad90630f7528f
nulla2011 <nulla@outlook.com>
2025-08-31 11:36:32 +0800

    fix

--- Script/ai_sites.mjs
+++ Script/ai_sites.mjs
@@ -5,12 +5,14 @@ const main = () => {
   let rules, rm, shared
   try {
     rules = { payload: readFileSync('../Temp/ai.txt', 'utf-8').split('\n').filter(t => t && !t.startsWith('#')) }
+    add = parse(readFileSync(`../Patches/Add/AI.yaml`, "utf8"));
     rm = parse(readFileSync(`../Patches/Remove/AI.yaml`, "utf8"));
     shared = parse(readFileSync(`../Patches/Remove/Shared.yaml`, "utf8"));
   }
   catch (e) {
     console.error(e)
   }
+  rules.payload = rules.payload.concat(add)
   rules.payload = rules.payload.filter(r => !rm.some(e => r.includes(e)))
   rules.payload = rules.payload.filter(r => !shared.some(e => r.includes(e)))
   try {
